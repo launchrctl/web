da76c2188d300954ba4fddf82a3cdc7b3b94fda9
Konstantin Shupenko <koskinparkin@gmail.com>
2024-07-22 13:53:53 +0600

    Fix websokets apiUrl. #74

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import env from 'vite-plugin-env-compatible'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), env()],
 })
